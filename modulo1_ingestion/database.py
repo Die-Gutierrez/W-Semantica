6b672fd24a76580d2d89dd406dd57bb9ab31e152
Dennis Calderon <dscalderonm@unjbg.edu.pe>
2026-05-11 09:46:19 -0500

    Eliminando referencia innecesaria

--- modulo1_ingestion/database.py
+++ modulo1_ingestion/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_all_models, Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
